a2e18e7ca91a4795b4cb7bf163cb83d8c50ff760
Muhammad Junaid <mjunaid.zafar01@gmail.com>
2026-01-20 17:45:27 +0500

    Integrate animated splash screen in App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {StyleSheet} from 'react-native';
 import BootSplash from 'react-native-bootsplash';
 import {RootNavigator} from './src/navigation/RootNavigator';
+import {SplashScreen} from './src/components/SplashScreen';
 
 function App(): React.JSX.Element {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashComplete = useCallback(() => {
+    setShowSplash(false);
+  }, []);
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
@@ -16,6 +23,7 @@ function App(): React.JSX.Element {
           }}>
           <RootNavigator />
         </NavigationContainer>
+        {showSplash && <SplashScreen onAnimationComplete={handleSplashComplete} />}
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
